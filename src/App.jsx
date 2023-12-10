ac095c11cfd61bfc1227853df0b25d070d1ba451
yasu2122yasu <ya2su1shi2@icloud.com>
2023-12-10 22:32:41 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './styles.css';
+import { useState, useCallback, useMemo } from 'react';
+import { ChildArea } from './ChildArea';
+
+export default function App() {
+  console.log('App');
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState('');
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => {
+    setOpen(!open);
+  };
+
+  const onClickClose = useCallback(() => setOpen(false), [open]);
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
+    </div>
+  );
+}
