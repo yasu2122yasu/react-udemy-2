76223e209a8065c3222bcb2c574a5636dc5b0ca4
yasu2122yasu <ya2su1shi2@icloud.com>
2023-12-12 00:40:20 +0900

    make pull requeset

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
 import { InlineStyle } from './components/inlineStyle';
 import { CssModules } from './components/CssModules';
 import './styles.css';
 import { StyledJsx } from './components/StyledJsx';
 import { StyledComponents } from './components/StyledComponents';
 import { Emotion } from './components/Emotion';
+import { Home } from './Home';
+import { Page1 } from './Page1';
+import { Page2 } from './Page2';
 
 export default function App() {
   return (
-    <div className="App">
-      <InlineStyle />
-      <CssModules />
-      <StyledJsx />
-      <StyledComponents />
-      <Emotion />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <InlineStyle />
+        <CssModules />
+        <StyledJsx />
+        <StyledComponents />
+        <Emotion />
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+        <br />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/page1" element={<Page1 />} />
+          <Route path="/page2" element={<Page2 />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
