c3f304e1eb194c28eced020f50572d06e7942d18
yasu2122yasu <ya2su1shi2@icloud.com>
2023-12-12 18:47:39 +0900

    make pull requeset

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,10 +1,14 @@
 import { Link, Outlet } from 'react-router-dom';
 
 export const Page1 = () => {
+  //   100件の配列をstateとして渡す
+  const arr = [...Array(100).keys()];
+  console.log(arr);
+
   return (
     <div>
       <h1>Page1ページです</h1>
-      <Link to="/page1/detailA">DetailA</Link>
+      <Link to={{ pathname: '/page1/detailA', state: arr }}>DetailA</Link>
       <br />
       <Link to="/page1/detailB">DetailB</Link>
       <Outlet />
